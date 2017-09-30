7b9342aea0ecc1b24c5f9ee5bc2299a88a22871d
Alex Hall <alex.mojaki@gmail.com>
2017-09-30 13:10:16 +0200

    Bump to version 0.1.8

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ if version_info[0] == 2:
     install_requires += ['backports.functools_lru_cache']
 
 setup(name='birdseye',
-      version='0.1.7',
+      version='0.1.8',
       description='Python debugger using the AST',
       classifiers=[
           'License :: OSI Approved :: MIT License',
