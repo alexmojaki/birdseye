f1f626d4f488a9c94ce7f23c8587e858683b4b37
Alex Hall <alex.mojaki@gmail.com>
2017-10-18 23:58:33 +0200

    Set database location when running server in travis test

--- misc/travis_test.sh
+++ misc/travis_test.sh
@@ -7,9 +7,9 @@ pip install -e .
 rm ~/.birdseye_test.db || true
 
 export BIRDSEYE_SERVER_RUNNING=true
-
+export BIRDSEYE_DB=sqlite:///$HOME/.birdseye_test.db
 gunicorn -b 127.0.0.1:7777 birdseye.server:app &
-sleep 3
+sleep 1
 
 set +e
 
