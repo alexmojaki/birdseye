0aad86e1842780a12038927510cf130ee38632fa
Alex Hall <alex.mojaki@gmail.com>
2017-10-19 19:36:31 +0200

    Clear test db in python for easy local testing

--- tests/__init__.py
+++ tests/__init__.py
@@ -1,4 +1,9 @@
 import os
 
-os.environ['BIRDSEYE_DB'] = 'sqlite:///' + os.path.join(os.path.expanduser('~'),
-                                                        '.birdseye_test.db')
+path = os.path.join(os.path.expanduser('~'), '.birdseye_test.db')
+
+# Remove the database to start from scratch
+if os.path.exists(path):
+    os.remove(path)
+
+os.environ['BIRDSEYE_DB'] = 'sqlite:///' + path
--- tests/test_interface.py
+++ tests/test_interface.py
@@ -38,7 +38,7 @@ class TestInterface(unittest.TestCase):
     def setUp(self):
         self.driver = webdriver.PhantomJS(service_args=['--webdriver-loglevel=DEBUG'])
         self.driver.set_window_size(1400, 1000)
-        self.driver.implicitly_wait(0.3)
+        self.driver.implicitly_wait(0.5)
         if not os.environ.get('BIRDSEYE_SERVER_RUNNING'):
             Thread(target=lambda: app.run(port=7777)).start()
             sleep(1)
