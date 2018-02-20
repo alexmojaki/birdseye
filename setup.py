f12ec8ac7516d8d0c68f477eae4fd1df6b1a7b17
Alex Hall <alex.mojaki@gmail.com>
2018-02-20 14:50:29 +0200

    Bump to 0.2.4

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ if version_info[0] == 2:
                          'typing']
 
 setup(name='birdseye',
-      version='0.2.3',
+      version='0.2.4',
       description='Quick, convenient, expression-centric, graphical Python debugger using the AST',
       classifiers=[
           'License :: OSI Approved :: MIT License',
