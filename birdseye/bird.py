7edbfb6f4b8f3f97534578f176318f82e3b584d8
Aivar Annamaa <aivarannamaa@users.noreply.github.com>
2020-04-13 13:40:44 +0300

    Allow any Exception when importing optional modules
    Importing a module may fail with other exceptions besides ImportError, eg. with OSError when the module is present in user site packages but for wrong architecture.
    
    Here is an example case which would be solved by this PR: https://github.com/thonny/thonny/issues/1141

--- birdseye/bird.py
+++ birdseye/bird.py
@@ -40,13 +40,13 @@ from birdseye import __version__
 
 try:
     from numpy import ndarray
-except ImportError:
+except Exception:
     class ndarray(object):
         pass
 
 try:
     from pandas import DataFrame, Series
-except ImportError:
+except Exception:
     class DataFrame(object):
         pass
 
@@ -56,7 +56,7 @@ except ImportError:
 
 try:
     from django.db.models import QuerySet
-except ImportError:
+except Exception:
     class QuerySet(object):
         pass
 
