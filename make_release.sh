435fbdc370792a325f56416373217b0d5d7b276f
Alex Hall <alex.mojaki@gmail.com>
2021-07-03 13:58:05 +0200

    Update wheel building stuff

--- make_release.sh
+++ make_release.sh
@@ -26,5 +26,5 @@ export TAG="v${1}"
 git tag "${TAG}"
 git push origin master "${TAG}"
 rm -rf ./build ./dist
-python3 -m pep517.build -bs .
+python -m build --sdist --wheel .
 twine upload ./dist/*.whl dist/*.tar.gz
